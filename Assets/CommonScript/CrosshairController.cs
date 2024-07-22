a223a24a4ee5b996c743e05d08a403311c49de93
Koraphat-Panunta <mark_nanza@hotmail.com>
2024-07-22 17:14:35 +0700

    Add Crosshair

---
+++ Assets/CommonScript/CrosshairController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairController : MonoBehaviour
+{
+    public RectTransform Crosshair_lineUp;
+    public RectTransform Crosshair_lineDown;
+    public RectTransform Crosshair_lineLeft;
+    public RectTransform Crosshair_lineRight;
+    public RectTransform Crosshair_Position;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
