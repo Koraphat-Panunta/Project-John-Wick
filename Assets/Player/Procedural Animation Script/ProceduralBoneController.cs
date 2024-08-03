7ccbd519704ec127475969b4f00235b5071cac50
Koraphat-Panunta <mark_nanza@hotmail.com>
2024-08-03 16:08:46 +0700

    3/8/67 1608

--- Assets/Player/Procedural Animation Script/ProceduralBoneController.cs
+++ Assets/Player/Procedural Animation Script/ProceduralBoneController.cs
@@ -10,6 +10,7 @@ public class ProceduralBoneController : MonoBehaviour
     [SerializeField] MultiAimConstraint Head;
     [SerializeField] MultiAimConstraint RightArm;
     [SerializeField] MultiAimConstraint RightHand;
+    public GameObject sourceTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,25 @@ public class ProceduralBoneController : MonoBehaviour
         Head.weight = w;
         RightArm.weight = w;
         RightHand.weight = w;
+    }
+    public void SetSourceTarget(GameObject gameObject)
+    {
+        SetConstraintSource(Spine, gameObject);
+        SetConstraintSource(Head, gameObject);
+        SetConstraintSource(RightArm, gameObject);
+        SetConstraintSource(RightHand, gameObject);
+    }
+    private void SetConstraintSource(MultiAimConstraint constraint, GameObject source)
+    {
+        var data = constraint.data.sourceObjects;
+        data.Clear();
+
+        // Create a new constraint source
+        WeightedTransform sourceTransform = new WeightedTransform(source.transform, 1.0f);
+        data.Add(sourceTransform);
+
+        // Apply the updated source objects
+        constraint.data.sourceObjects = data;
     }
     private void OnEnable()
     {
