a9b18f2a3fa2f9597fb46854272fcf1bfbe73ed7
Koraphat-Panunta <mark_nanza@hotmail.com>
2024-10-28 12:55:27 +0700

    28/10/67 1255

--- Assets/Weapon/WeaponScript/WeaponCommand/PullTriggerCommand.cs
+++ Assets/Weapon/WeaponScript/WeaponCommand/PullTriggerCommand.cs
@@ -10,7 +10,7 @@ public class PullTriggerCommand : WeaponCommand
     public override void Execute()
     {
         if (weapon.weapon_stateManager._currentState != weapon.weapon_stateManager.reloadState
-            &&weapon.weapon_StanceManager._currentStance == weapon.weapon_StanceManager.aimDownSight )
+            &&weapon.weapon_StanceManager._currentStance == weapon.weapon_StanceManager.aimDownSight&&weapon.weapon_StanceManager.AimingWeight>=1 )
         {
             if (weapon.triggerPull == Weapon.TriggerPull.Up)
             {
