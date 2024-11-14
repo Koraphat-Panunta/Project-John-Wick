25532b3ec25f81c2badd29c7bf67be563d8da036
Koraphat Panunta <mark_nanza@hotmal.com>
2024-11-14 16:40:18 +0700

    14/11/67

--- Assets/Player/InputUpdate/PlayerInputBlackBoard.cs
+++ Assets/Player/InputUpdate/PlayerInputBlackBoard.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputBlackBoard : MonoBehaviour
 {
-    PlayerInput playerInput = new PlayerInput();
+    //PlayerInput playerInput = new PlayerInput();
 
     InputAction move;
     InputAction look;
@@ -25,17 +25,23 @@ public class PlayerInputBlackBoard : MonoBehaviour
     public bool pullTriggerInput;
     public bool reloadInput;
 
+    private void Awake()
+    {
+        //playerInput.Enable();
+        //move = playerInput.PlayerAction.Move;
+        //look = playerInput.PlayerAction.Look;
+        //sprint = playerInput.PlayerAction.Sprint;
+        //swapShoulder = playerInput.PlayerAction.SwapShoulder;
+        //switchWeapon = playerInput.PlayerAction.SwitchWeapon;
+        //aimDownSight = playerInput.PlayerAction.AimDownSight;
+        //pullTrigger = playerInput.PlayerAction.PullTrigger;
+        //reload = playerInput.PlayerAction.Reload;
+
+        InitailizedInput();
+    }
     void Start()
     {
-        playerInput.Enable();
-        move = playerInput.PlayerAction.Move;
-        look = playerInput.PlayerAction.Look;
-        sprint = playerInput.PlayerAction.Sprint;
-        swapShoulder = playerInput.PlayerAction.SwapShoulder;
-        switchWeapon = playerInput.PlayerAction.SwitchWeapon;
-        aimDownSight = playerInput.PlayerAction.AimDownSight;
-        pullTrigger = playerInput.PlayerAction.PullTrigger;
-        reload = playerInput.PlayerAction.Reload;
+       
     }
     private void InitailizedInput()
     {
@@ -45,7 +51,23 @@ public class PlayerInputBlackBoard : MonoBehaviour
         look.performed += context => lookInput = look.ReadValue<Vector2>();
         look.canceled += context => lookInput = Vector2.zero;
 
-        sprint.performed += context => sprintInput = sprint.ReadValue<bool>();
+        sprint.performed += context => sprintInput = true;
+        sprint.canceled += context => sprintInput = false;
+
+        swapShoulder.performed += context => swapShoulderInput = true;
+        swapShoulder.canceled += context => swapShoulderInput = false;
+
+        switchWeapon.performed += context => switchWeaponInput = true;
+        switchWeapon.canceled += context => switchWeaponInput = false;
+
+        aimDownSight.performed += context => aimDownSightInput = true;
+        aimDownSight.canceled += context => aimDownSightInput = false;
+
+        pullTrigger.performed += context => pullTriggerInput = true;
+        pullTrigger.canceled += context => pullTriggerInput = false;
+
+        reload.performed += context => reloadInput = true;
+        reload.canceled += context => reloadInput = false;
     }
 
    
