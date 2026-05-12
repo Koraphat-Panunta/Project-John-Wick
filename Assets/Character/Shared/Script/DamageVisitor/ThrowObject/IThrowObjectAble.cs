4c470b140f27db18e7600c283f1f40516c322dd2
Koraphat Panunta <mark_nanza@hotmail.com>
2026-05-13 04:09:18 +0700

    Connect claude mcp to unity

--- Assets/Character/Shared/Script/DamageVisitor/ThrowObject/IThrowObjectAble.cs
+++ Assets/Character/Shared/Script/DamageVisitor/ThrowObject/IThrowObjectAble.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public interface IThrowObjectAble 
 {
-   
     public IThrowAbleObject curThrowAbleObject { get; set; }
     public IBeenThrewObjectAt curBeenThrowObjectAt { get; set; }
     public void ObjectBeenHit(IThrowAbleObject threwObject,Collider hited);
